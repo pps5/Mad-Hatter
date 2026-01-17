3f48517c3706c94aa10cffb24dd6adbc8167fe3c
Naoki Nakajima <n2.jima@gmail.com>
2026-01-17 14:12:58 +0900

    Fix transaction editor dropdown imports

--- transaction/src/main/java/com/example/madhatter/transaction/TransactionEditorScreen.kt
+++ transaction/src/main/java/com/example/madhatter/transaction/TransactionEditorScreen.kt
@@ -14,12 +14,14 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.material3.ExposedDropdownMenuDefaults
 import androidx.compose.material3.FilterChip
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
